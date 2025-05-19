4fcc67091848060a7fdebafbc27352b2f70311de
FareenKhan904 <farheensamandar94@gmail.com>
2025-05-19 06:13:36 -0700

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export function  app(){
+
+const [count , setcount] = useState(0)
+
+function increment(){
+  setcount(count +1)
+}
+
+function decrement(){
+   if(count > 0)
+    {setcount(count -1)}
+
+   else{ 
+    alert("Count cannot be less than zero")
+  }
+}
+
+function reset(){
+  setcount(0)
+}
+
+
+return(
+  <>
+  <button onClick={increment}>Increment</button>
+  <button onClick={decrement}>decrement</button>
+  <button onClick={reset}>reset</button>
+  <p>{count}</p>
+  </>
+);
+}
+
+export default app;
